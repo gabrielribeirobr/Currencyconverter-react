a01a73c84b6bea7213df1cfca2418e0721332e7d
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-09-25 18:02:25 +0000

    feat: added api to get key for currenci and show as option on select

--- src/components/Boxvalue/Boxvalue.jsx
+++ src/components/Boxvalue/Boxvalue.jsx
@@ -1,17 +1,43 @@
-import { useState } from "react";
 import styles from "./boxvalue.module.css";
+import { useState, useEffect } from "react";
+
+export function useKeyCurrency() {
+  const [currencies, setCurrencies] = useState([]);
+  useEffect(() => {
+    async function getCurrencies() {
+      try {
+        const response = await fetch("https://v6.exchangerate-api.com/v6/1c7be34a277cb0073e935419/latest/USD");
+        const data = await response.json();
+        console.log(data);
+        const currencyKeys = Object.keys(data.conversion_rates);
+        setCurrencies(currencyKeys);
+      } catch (erro) {
+        console.error("Error ao buscar moerdas");
+      }
+    }
+    getCurrencies();
+  }, []);
+  return currencies;
+}
 
 export default function Boxvalue() {
-
-  const[value, setValue] = useState(1);
-
+  const [value, setValue] = useState(1);
+  const currencies = useKeyCurrency();
   return (
     <>
       <div className={styles.inputValue}>
-      <input
-      type="number"
-      value={value}
-      onChange={(e) => setValue(e.target.value)} />
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select className={styles.countryCurrency}>
+          {currencies.map((currency =>(
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          )))}
+        </select>
       </div>
     </>
   );
