a9577c434819e4fec07578332e43cbe648de78f3
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-11-19 18:34:03 +0000

    style: change color for blue main and adjust select swapCambial

--- src/components/Swapcambial/Swapcambial.jsx
+++ src/components/Swapcambial/Swapcambial.jsx
@@ -5,9 +5,10 @@ export default function Swapcambial({ selectedCurrency}) {
   const currencies = useKeyCurrency();
   return (
     <div className={styles.swapBox}>
-      <div className={styles.selectedCurrency}>{selectedCurrency}</div>
+      <div className={styles.currencySwap}>{selectedCurrency}</div>
       
-      <div className={styles.desiredCurrency}><select className={styles.countryCurrency}>{currencies.map((currency =>(
+      <div className={styles.currencySwap}>
+        <select className={styles.countryCurrency}>{currencies.map((currency =>(
             <option key={currency.code} value={currency.rate}>
               {currency.code}
             </option>
