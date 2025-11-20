8dc6fc40fa2fd2dd22a3fae75ac76787f56fcb45
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-11-20 17:14:51 +0000

    feat: create state for destinationCurrency

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,19 @@ import Swapcambial from "./components/Swapcambial/Swapcambial";
 export default function App() {
   const [selectedCurrency, setSelectedCurrency] = useState("EUR");
   const [amount, setAmount] = useState(1);
+  const [destinationCurrency, setDestinationCurrency] = useState("");
+  const handleChange = (e) => {
+        setDestinationCurrency((e).target.value);                
+      }
+
+      console.log(destinationCurrency);
   return (
     <>
       <div>
         <Header />
         <Boxvalue selectedCurrency={selectedCurrency}
         setSelectedCurrency={setSelectedCurrency} amount={amount} setAmount={setAmount} />
-        <Swapcambial selectedCurrency={selectedCurrency} amount={amount}/>
+        <Swapcambial selectedCurrency={selectedCurrency} amount={amount} handleChange={handleChange} destinationCurrency={destinationCurrency}/>
       </div>
       
     </>
