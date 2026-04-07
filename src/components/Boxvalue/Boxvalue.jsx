df01fe83830fd612d146868e4a2560b39b16deb0
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2026-04-07 18:04:57 +0100

    fix: add proxy to allow CORS

--- src/components/Boxvalue/Boxvalue.jsx
+++ src/components/Boxvalue/Boxvalue.jsx
@@ -7,19 +7,16 @@ export function useKeyCurrency() {
   useEffect(() => {
     async function fetchCurrencies() {
       try {
-        
-        const responseNames = await fetch("https://api.frankfurter.app/currencies");
+        const responseNames = await fetch("/api/currencies");
         const dataNames = await responseNames.json();
 
-        
-        const responseRates = await fetch("https://api.frankfurter.app/latest");
+        const responseRates = await fetch("/api/latest");
         const dataRates = await responseRates.json();
 
-        
         const currencyArray = Object.entries(dataNames).map(([code, name]) => ({
           code,
           name,
-          rate: dataRates.rates[code] || 1, 
+          rate: dataRates.rates[code] || 1,
         }));
 
         setCurrencies(currencyArray);
@@ -28,41 +25,42 @@ export function useKeyCurrency() {
       }
     }
 
-    fetchCurrencies(); 
+    fetchCurrencies();
   }, []);
 
   return currencies;
 }
 
-export default function Boxvalue({ selectedCurrency, onCurrencyChange , amount, onAmountChange }) {
+export default function Boxvalue({
+  selectedCurrency,
+  onCurrencyChange,
+  amount,
+  onAmountChange,
+}) {
   const currencies = useKeyCurrency();
 
   return (
     <div className={styles.box}>
-  <label>Amount</label>
+      <label>Amount</label>
 
-  <div className={styles.inputGroup}>
-    <input
-      type="number"
-      value={amount}
-      onChange={(e) => onAmountChange(Number(e.target.value))}
-    />
+      <div className={styles.inputGroup}>
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => onAmountChange(Number(e.target.value))}
+        />
 
-    <select
-      value={selectedCurrency}
-      onChange={(e) => onCurrencyChange(e.target.value)}
-    >
-      {currencies.map(currency => (
-        <option key={currency.code} value={currency.code}>
-          {currency.code} - {currency.name}
-        </option>
-      ))}
-    </select>
-  </div>
-</div>
-
+        <select
+          value={selectedCurrency}
+          onChange={(e) => onCurrencyChange(e.target.value)}
+        >
+          {currencies.map((currency) => (
+            <option key={currency.code} value={currency.code}>
+              {currency.code} - {currency.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
-
-
-
